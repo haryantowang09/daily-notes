1a0588a102981f139a42f5d25f3079290af23ffa
Haryanto <haryantowang09@gmail.com>
2019-06-08 15:29:23 +0700

    Add : Antd

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
+import { Button } from 'ant-design-vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import 'ant-design-vue/dist/antd.css'
 
+Vue.use(Button)
 Vue.config.productionTip = false
 
 new Vue({
