a6978be367ac8ed23ed462037c6b3931574dd591
Haryanto <haryantowang09@gmail.com>
2019-06-09 20:29:58 +0700

    Add Layout

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
-import { Button } from 'ant-design-vue'
+// import { Button, Layout } from 'ant-design-vue'
+import Antd from 'ant-design-vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
 import 'ant-design-vue/dist/antd.css'
 
-Vue.use(Button)
+// Vue.use(Button, Layout)
+Vue.use(Antd)
 Vue.config.productionTip = false
 
 new Vue({
