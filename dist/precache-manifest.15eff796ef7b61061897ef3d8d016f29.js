c56a8a3d693a288d30fa12d2d2d0d19ee81e8432
Haryanto <haryantowang09@gmail.com>
2019-06-09 10:07:42 +0700

    Initial dist subtree commit

---
+++ dist/precache-manifest.15eff796ef7b61061897ef3d8d016f29.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    'revision': 'a917989e03a279f59ea8',
+    'url': '/<wang-diary>/js/about~2653ddd4.0d8bf0d0.js'
+  },
+  {
+    'revision': 'e639870a8e78f9d6c229',
+    'url': '/<wang-diary>/css/app~d0ae3f07.412f32b8.css'
+  },
+  {
+    'revision': 'e639870a8e78f9d6c229',
+    'url': '/<wang-diary>/js/app~d0ae3f07.a44a7172.js'
+  },
+  {
+    'revision': '8fa055b5a6e4f383168e',
+    'url': '/<wang-diary>/js/chunk-vendors~0605657e.5ecf762e.js'
+  },
+  {
+    'revision': 'b035bcbd9f431245caa2',
+    'url': '/<wang-diary>/js/chunk-vendors~0b3b47da.4efe344e.js'
+  },
+  {
+    'revision': '6d1bd3fb2ef2ec93931d',
+    'url': '/<wang-diary>/js/chunk-vendors~253ae210.49c8a816.js'
+  },
+  {
+    'revision': '334d8a0b1404e8404d5c',
+    'url': '/<wang-diary>/js/chunk-vendors~2b13b260.153fbb2c.js'
+  },
+  {
+    'revision': '6b2bb8f13e1695957fee',
+    'url': '/<wang-diary>/js/chunk-vendors~678f84af.0a6a0d44.js'
+  },
+  {
+    'revision': 'eca8573cac11fb5ed846',
+    'url': '/<wang-diary>/js/chunk-vendors~690b702c.f7ba4430.js'
+  },
+  {
+    'revision': 'c9d77a031aad7b14024b',
+    'url': '/<wang-diary>/css/chunk-vendors~833e20b8.c4429509.css'
+  },
+  {
+    'revision': 'c9d77a031aad7b14024b',
+    'url': '/<wang-diary>/js/chunk-vendors~833e20b8.8ae3cce5.js'
+  },
+  {
+    'revision': 'a88df14c53940fd67ec5',
+    'url': '/<wang-diary>/js/chunk-vendors~d2305125.bee7239c.js'
+  },
+  {
+    'revision': '05256d3ef576e21f7cfc',
+    'url': '/<wang-diary>/js/chunk-vendors~fdc6512a.7700381a.js'
+  },
+  {
+    'revision': '82b9c7a5a3f405032b1db71a25f67021',
+    'url': '/<wang-diary>/img/logo.82b9c7a5.png'
+  },
+  {
+    'revision': 'a63a666f3965dac28b5c2f7f0e71db20',
+    'url': '/<wang-diary>/index.html'
+  },
+  {
+    'revision': 'b6216d61c03e6ce0c9aea6ca7808f7ca',
+    'url': '/<wang-diary>/robots.txt'
+  }
+]
